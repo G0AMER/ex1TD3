fc8ca728ba7c2983fea865137b8f26e94375c81f
G.AMER <gargouri.ameur@enis.tn>
2024-04-15 14:14:38 +0100

    a

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        //1ère étape :créer le pipeline (tab de sort de taille fixe)
+        int n=5;
+        Sort pipeline []=new Sort[n];
+
+    }
+}
