ef3a78277b2766425f645b564d3aba624faddd58
G.AMER <gargouri.ameur@enis.tn>
2024-04-15 14:48:20 +0100

    15/04/2024 14:48

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import java.util.concurrent.SynchronousQueue;
+
 public class Main {
     public static void main(String[] args) {
         //1ère étape :créer le pipeline (tab de sort de taille fixe)
-        int n=5;
-        Sort pipeline []=new Sort[n];
-
+        int N = 5;
+        Sort[] pipeline = new Sort[N];
+        SynchronousQueue<Integer> I = new SynchronousQueue<Integer>();
+        SynchronousQueue<Integer> O = new SynchronousQueue<Integer>();
+        for (int i = 0; i < N; i++) {
+            pipeline[i] = new Sort(i, I, O, N);
+            I = O;
+            SynchronousQueue<Integer> O = new SynchronousQueue<Integer>();
+            pipeline[i].start();
+        }
     }
 }
--- src/Sort.java
+++ src/Sort.java
@@ -1,2 +1,44 @@
-public class Sort {
+import java.util.concurrent.SynchronousQueue;
+
+public class Sort extends Thread {
+    SynchronousQueue<Integer> O = new SynchronousQueue<Integer>();
+    int N;
+    SynchronousQueue<Integer> I = new SynchronousQueue<Integer>();
+    int i;
+    int[] Res;
+
+    public Sort(int i, SynchronousQueue<Integer> I, SynchronousQueue<Integer> O, int N) {
+        this.i = i;
+        this.I = I;
+        this.O = O;
+        this.N = N;
+        Res = new int[N];
+    }
+
+    @Override
+    public void run() {
+        int min;
+        if (i == 0) {
+            int[] tab = {12, 0, 5, 8, 3};
+            min = tab[0];
+            for (int j = 1; j < N; j++) {
+                if (tab[j] < min) {
+                    try {
+                        O.put(min);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    min = tab[j];
+                } else {
+                    try {
+                        O.put(tab[i]);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+            }
+            Res[i] = min;
+        }
+    }
 }
